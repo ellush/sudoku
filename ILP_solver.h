4a63f488855aef1638d1c60bd203624e9f47f982
ellush <ellalevin4@gmail.com>
2019-09-16 00:05:51 +0300

    Deleted old ILP_solver file (moved to LP.c/.h)

--- ILP_solver.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "Game_Board.h"
-#include "gurobi_c.h"
-#include <string.h>
-
-bool ILP_solver(Board B, int n, int m, bool apply, bool ILP);
